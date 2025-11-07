bcf5bc498bd6b02fe576e200b3bbae07f7d89644
Rami Abu Hashish <127823084+RGorovoi@users.noreply.github.com>
2025-11-07 00:31:06 -0500

    Time: 251 ms (70.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ Week-5/176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE salary < (
+        SELECT MAX(salary)
+        FROM Employee
+    )
+)  
+AS SecondHighestSalary
